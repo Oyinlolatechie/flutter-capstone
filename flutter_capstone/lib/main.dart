7c1ee3857eaf4a4c4fdea08bb310edd97a592cd3
Oyinlolatechie <adepitanoyinlola4@gmail.com>
2024-02-11 20:22:52 +0100

    feat: add the hero image

--- flutter_capstone/lib/main.dart
+++ flutter_capstone/lib/main.dart
@@ -18,13 +18,23 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 10.0),
+                padding: const EdgeInsets.only(left: 30.0),
                 child: Image.asset("assets/arrow-left.jpg"),
               ),
               const Text("Detail"),
               Padding(
-                padding: const EdgeInsets.only(right: 10.0),
+                padding: const EdgeInsets.only(right: 30.0),
                 child: Image.asset("assets/Heart.jpg"),
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Image.asset("assets/Rectangle.jpg"),
               ),
             ],
           ),
